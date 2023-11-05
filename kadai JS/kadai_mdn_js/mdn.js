ddaebe5dadffca79a269cd7ae2cde3a637f578f1
DokuDokuringo <147831478+DokuDokuringo@users.noreply.github.com>
2023-11-05 15:43:04 +0900

    Update mdn.js

--- kadai JS/kadai_mdn_js/mdn.js
+++ kadai JS/kadai_mdn_js/mdn.js
@@ -1,10 +1,7 @@
 const moonLanding = new Date();
 const year = moonLanding.getFullYear();
-
 const monTh = new Date();
-const month = monTh.getMonth()
-
+const month = monTh.getMonth() + 1;
 const birthday = new Date();
 const date = birthday.getDate();
-
 console.log(year + '年' + month + '月' + date + '日');
